3bbfad18b3ab9283f66bbdb5dca293606008d750
samuelIkoli <ayibanimiikoli@gmail.com>
2025-02-05 14:15:41 +0100

    added cron job

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/robfig/cron"
 	"github.com/samuelIkoli/fun_numbers/routes"
 )
 
 func main(){
 
+	c := cron.New()
+	c.AddFunc("*/500 * * * *", func() {
+		fmt.Println("Cronning")
+		http.Get("https://hng12-be-0.onrender.com/")
+
+	})
+	c.Start()
 
 	server:= gin.Default()
 
